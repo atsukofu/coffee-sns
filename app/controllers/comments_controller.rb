11dcf2ef47d36a0eec2a163e14337f709f2c96ac
atsukofu <atsukofu0527@gmail.com>
2020-05-24 00:41:11 +0900

    add redirect page movement

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,18 @@
 class CommentsController < ApplicationController
+  before_action :set_group
+  
   def create
     Comment.create(comment_params)
-    redirect_to "/comments/#{comment.post.id}"
+    redirect_to post_path(@post.id)
   end
 
   private
   def comment_params
     params.require(:comment).permit(:content).merge(post_id: params[:post_id], user_id: current_user.id)
   end
+
+  def set_group
+    @post = Post.find(params[:post_id])
+  end
+
 end
