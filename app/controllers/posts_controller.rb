cd856d3f80fdd90c12e5f6ef99a3023d06e3a716
atsukofu <atsukofu0527@gmail.com>
2020-05-23 23:46:19 +0900

    add post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,26 @@
 class PostsController < ApplicationController
 
   def index
-   
+   @posts = Post.all
   end
 
   def show
-    
+    @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:product, :shop, :brand, :price, :per, :stars, :acidity, :bitterness, :sweetness, :fragrance, :richiness).merge(user_id: current_user.id)
+  end
+
+  
 end
