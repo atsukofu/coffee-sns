6a53d0bd7505d7fcd403898393105b73f0eefce6
atsukofu <atsukofu0527@gmail.com>
2020-05-25 23:26:05 +0900

    add user picture register function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :message])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :message ,:image])
   end
 
 end
