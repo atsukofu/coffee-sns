12869fcdf6553494d2207acf6069cc2092619e0f
atsukofu <atsukofu0527@gmail.com>
2020-06-07 22:52:43 +0900

    modified user register function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,9 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :message ,:image])
-  end
-
-  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :message ,:image])
   end
+
 
   def redirect_root
     if current_user == nil
