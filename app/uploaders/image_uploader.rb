ba4fe8d351a3652e58f8953aac0cdd88448e518c
atsukofu <atsukofu0527@gmail.com>
2020-06-11 23:20:39 +0900

    add carrierwave setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -55,9 +55,10 @@ class ImageUploader < CarrierWave::Uploader::Base
       config.fog_provider = 'fog/aws'
       config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-        aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-        region: 'ap-northeast-1'
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: ENV['AWS_DEFAULT_REGION'],
+        path_style: true
       }
     end
   end
