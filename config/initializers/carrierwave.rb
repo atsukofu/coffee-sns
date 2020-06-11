c14cadb17d39d93420c579daa00bff08b3ff8fa4
atsukofu <atsukofu0527@gmail.com>
2020-06-11 21:43:37 +0900

    modified carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,18 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
+
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test
+    config.storage = :file
+  elsif Rails.env.production? 
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: 'ap-northeast-1'
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1'
     }
 
   config.fog_directory  = 'coffee-sns'
